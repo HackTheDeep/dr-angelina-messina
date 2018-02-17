500c26cc0fbe18bc83f9a6406c8e0b9fe8444680
Annie Wang <anwang911@gmail.com>
2018-02-17 15:38:45 -0500

    created game.js

---
+++ game.js
@@ -0,0 +1,20 @@
+function popUp_amnh() {
+    document.getElementById("amnh").style.display='none';
+    alert ("In 1941 Dr. Angelina Messina was appointed Associate Curator in the Department of Micropaleontology of the American Museum of Natural History.");
+}
+function popUp_catalog() {
+    document.getElementById("catalog").style.display='none';
+    alert ("In 1928 Dr. Angelina Messina helped systemize or catalog the literature of the foraminifera (single-celled marine organisms). It was called the Foraminifera and Ostracoda Microfossil Catalog.");
+}
+function popUp_membership() {
+    document.getElementById("membership").style.display='none';
+    alert ("Dr. Messina was a member of Science Societies in the U.S., Italy, Germany, and Japan and a part of the Science Honor Society of Sigma Xi.");
+}
+function popUp_boat() {
+    document.getElementById("boat").style.display='none';
+    alert ("Dr. Messina spent part of her time in France on a boat studying marine fossil formation of the coastal formation.");
+}
+function popUp_oil() {
+    document.getElementById("oil").style.display='none';
+    alert ("Dr. Messina’s research in the study on foraminifera contributed to oil mining.");
+}
